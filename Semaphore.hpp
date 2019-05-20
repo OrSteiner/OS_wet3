b1518e8026f0d4155ac43c6f665cdaf087457757
amitofir <amitofir@gmail.com>
2019-05-20 15:27:28 +0300

    working semaphore

--- Semaphore.hpp
+++ Semaphore.hpp
@@ -7,14 +7,15 @@ class Semaphore {
 public:
 	Semaphore(); // Constructs a new semaphore with a counter of 0
 	Semaphore(unsigned val); // Constructs a new semaphore with a counter of val
-
+    ~Semaphore();
 
 	void up(); // Mark: 1 Thread has left the critical section
 	void down(); // Block untill counter >0, and mark - One thread has entered the critical section.
 
 private:
-	int value;
-	pth
+	int size;
+	pthread_mutex_t mutex;
+    pthread_cond_t cond;
 	// TODO 
 };
 
