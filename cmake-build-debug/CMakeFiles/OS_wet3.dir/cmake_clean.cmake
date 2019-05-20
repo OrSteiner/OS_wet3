612c4485777acfaade89d0f6d4962a2c957ae2d1
OrSteiner <39240606+OrSteiner@users.noreply.github.com>
2019-05-20 11:48:03 +0300

    part1

---
+++ cmake-build-debug/CMakeFiles/OS_wet3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_wet3.dir/Part1/PCQueue.cpp.obj"
+  "OS_wet3.pdb"
+  "OS_wet3.exe"
+  "OS_wet3.exe.manifest"
+  "libOS_wet3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_wet3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
