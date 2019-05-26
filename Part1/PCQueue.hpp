39bc46b1223f8b4b40df846f368d36cba2bacf62
OrSteiner <39240606+OrSteiner@users.noreply.github.com>
2019-05-26 22:34:53 +0300

    new PCQ

--- Part1/PCQueue.hpp
+++ Part1/PCQueue.hpp
@@ -16,21 +16,72 @@ public:
 	// thread to enter and remove an item from the front of the queue and return it. 
 	// Assumes multiple consumers.
 	T pop(){
+		/*
+		 * Preventing empty queue access.
+		 */
+		qsize.down();
 
+		/*
+		 * We want to make sure, that only up to one reader and one writer will be able to wait on critical mutex.
+		 */
+		pthread_mutex_lock(&cond_mutex);
+		while(writer || reader){
+			pthread_cond_wait(&waiting_writer, &cond_mutex);
+		}
+		reader = true;
+		pthread_mutex_unlock(&cond_mutex);
+		/*
+		 * Critical section.
+		 */
+		pthread_mutex_lock(&critical_mutex);
+		T item = pc_queue.front();
+		pc_queue.pop();
+		pthread_mutex_unlock(&critical_mutex);
+		/*
+		 * Enabling the next reader to come in and compete for critical part with the writer (if waiting).
+		 */
+		pthread_mutex_lock(&cond_mutex);
+		reader = false;
+		pthread_cond_broadcast(&cond_mutex);
+		pthread_mutex_unlock(&cond_mutex);
+		return item;
 	};
 
 	// Allows for producer to enter with *minimal delay* and push items to back of the queue.
 	// Hint for *minimal delay* - Allow the consumers to delay the producer as little as possible.  
 	// Assumes single producer 
-	void push(const T& item); 
+	void push(const T& item){
+		/*
+		 * Anouncing writers arrival.
+		 */
+		pthread_mutex_lock(&cond_mutex);
+		writer = true;
+		pthread_mutex_unlock(&cond_mutex);
+		/*
+		 * Critical section.
+		 */
+		pthread_mutex_lock(&critical_mutex);
+		pc_queue.push(item);
+		pthread_mutex_unlock(&critical_mutex);
+
+		qsize.up();
+		/*
+		 * Releasing the readers from wating queue.
+		 */
+		pthread_mutex_lock(&cond_mutex);
+		writer = false;
+		pthread_cond_broadcast(&cond_mutex);
+		pthread_mutex_unlock(&cond_mutex);
+	};
 
 
 private:
-	queue pc_queue;
-	Semaphore qsize;
-	pthread_mutex_t critical_mutex;
-	pthread_cond_t waiting_writer;
-	int writers_num;
+	queue pc_queue;						// The tasks queue.
+	Semaphore qsize;					// Semaphore for preventing access to empty queue.
+	pthread_mutex_t critical_mutex;		// Critical section lock - Max of one reader and one writer will wait here.
+	pthread_mutex_t cond_mutex;			// The lock for conditions changes.
+	pthread_cond_t waiting_writer;		// Cond var for readers wainting queue.
+	bool writer, reader;				// Is there a reader/writer in action.
 
 };
 // Recommendation: Use the implementation of the std::queue for this exercise
